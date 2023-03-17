938ce4f942bcabe3a7e127100923f575a2f1ed48
Farzaneh Ansari <farzaneh.ansari@infoteam.de>
2023-03-17 10:15:57 +0100

    Add Color Generator

---
+++ ThemeDemo.Theme.ColorsGenerator/BackgroundThemeColor.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ThemeDemo.Theme.ColorsGenerator
+{
+    /// <summary>
+    /// Interaction logic for BackgroundThemeColor.xaml
+    /// </summary>
+    public partial class BackgroundThemeColor : UserControl
+    {
+        public BackgroundThemeColor()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ThemeDemo.Theme.ColorsGenerator/IpipeColors.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace ThemeDemo.Theme.ColorsGenerator
+{
+    public static class IpipeColors
+    {
+        readonly static  ResourceDictionary dictionary = new ResourceDictionary();
+       // static string uriString = "pack://application:,,,/ThemeDemo.Theme.Spring;component/ResourceDictionary/ColorResources.xaml";
+       static string uriString = "pack://application:,,,/ThemeDemo.Theme.ColorsGenerator;component/ResourceDictionary/DefaultColorTheme.xaml";
+
+        //Change the color as dynamic resource
+        private static Color ApplicationBackground => Colors.Peru; //(Color)dictionary[nameof(ApplicationBackground)];
+        private static Color ApplicationForeground => (Color)dictionary[nameof(ApplicationForeground)];
+        private static Color VortalBaseColor1 => (Color)dictionary[nameof(VortalBaseColor1)];
+        private static Color VortalBaseColor3 => (Color)dictionary[nameof(VortalBaseColor3)];
+        static IpipeColors()
+        {
+            dictionary.Source = new Uri(uriString, UriKind.RelativeOrAbsolute);
+
+            dictionary[nameof(ApplicationBackgroundBrush)] = new SolidColorBrush(ApplicationBackground);
+            dictionary[nameof(ApplicationForegroundBrush)] = new SolidColorBrush(ApplicationForeground);
+            dictionary[nameof(HeaderGradientBrush)] = new LinearGradientBrush(VortalBaseColor1, VortalBaseColor3, new Point(0, 0), new Point(0, 1));
+        }      
+
+        public static SolidColorBrush ApplicationForegroundBrush => (SolidColorBrush)dictionary[nameof(ApplicationForegroundBrush)];
+        public static SolidColorBrush ApplicationBackgroundBrush => (SolidColorBrush)dictionary[nameof(ApplicationBackgroundBrush)];
+        public static GradientBrush HeaderGradientBrush => (GradientBrush)dictionary[nameof(HeaderGradientBrush)];
+      
+    }
+}
